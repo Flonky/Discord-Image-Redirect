ab0f5cd453bd950cef03bab4f05f406db4c16692
Flonky <64008721+Flonky@users.noreply.github.com>
2022-04-13 20:07:21 +0200

    re add demo.jpeg

--- workers/workers.js
+++ workers/workers.js
@@ -1,10 +1,8 @@
 async function handleRequest(request) {
     const userAgent = request.headers.get('User-Agent') || '';
-    if (userAgent.includes('discordapp')) {
-      return Response.redirect('https://c.tenor.com/BP79uBTrSy0AAAAd/loading-discord.gif');
-    } else {
-      return Response.redirect('https://blog.flky.dev');
-    };
+    if (!userAgent.includes('discordapp')) {
+        return Response.redirect('https://blog.flky.dev');
+    }
   }
   
   addEventListener('fetch', event => {
