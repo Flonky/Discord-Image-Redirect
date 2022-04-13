05891b9f622d43d05402607994561bb6a44aac3c
Flonky <64008721+Flonky@users.noreply.github.com>
2022-04-13 19:58:50 +0200

    Update workers.js

--- workers/workers.js
+++ workers/workers.js
@@ -1,7 +1,7 @@
 async function handleRequest(request) {
     const userAgent = request.headers.get('User-Agent') || '';
     if (userAgent.includes('discordapp')) {
-      return Response.redirect('https://discorddemo.flky.dev/loading.gif');
+      return Response.redirect('https://c.tenor.com/BP79uBTrSy0AAAAd/loading-discord.gif');
     } else {
       return Response.redirect('https://blog.flky.dev');
     };
